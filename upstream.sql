38da7d604da63a9d8775adb937363b0e1491a1fa
abdelghafour77 <miso.abdo@gmail.com>
2022-04-16 21:27:30 +0000

    🛠

---
+++ upstream.sql
@@ -0,0 +1,185 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : sam. 16 avr. 2022 à 21:27
+-- Version du serveur :  8.0.25
+-- Version de PHP : 8.0.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `upstream`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `acting`
+--
+
+DROP TABLE IF EXISTS `acting`;
+CREATE TABLE IF NOT EXISTS `acting` (
+  `id_actor` int NOT NULL,
+  `id_movie` int NOT NULL,
+  PRIMARY KEY (`id_actor`,`id_movie`),
+  KEY `id_actor` (`id_actor`,`id_movie`),
+  KEY `id_movie` (`id_movie`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `actor`
+--
+
+DROP TABLE IF EXISTS `actor`;
+CREATE TABLE IF NOT EXISTS `actor` (
+  `id_actor` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(160) NOT NULL,
+  PRIMARY KEY (`id_actor`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE IF NOT EXISTS `category` (
+  `id_category` int NOT NULL,
+  `name` varchar(150) NOT NULL,
+  PRIMARY KEY (`id_category`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `category`
+--
+
+INSERT INTO `category` (`id_category`, `name`) VALUES
+(1, 'Action'),
+(2, 'Crime'),
+(3, 'Comedy'),
+(4, 'Adventures'),
+(5, 'Horror'),
+(6, 'Drama'),
+(7, 'Fantasia'),
+(8, 'Fiction'),
+(9, 'Romantic');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `contain`
+--
+
+DROP TABLE IF EXISTS `contain`;
+CREATE TABLE IF NOT EXISTS `contain` (
+  `id_movie` int NOT NULL,
+  `id_category` int NOT NULL,
+  KEY `id_movie` (`id_movie`),
+  KEY `id_category` (`id_category`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `language`
+--
+
+DROP TABLE IF EXISTS `language`;
+CREATE TABLE IF NOT EXISTS `language` (
+  `id_language` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL,
+  PRIMARY KEY (`id_language`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `language`
+--
+
+INSERT INTO `language` (`id_language`, `name`) VALUES
+(1, 'Arabic'),
+(2, 'English'),
+(3, 'Spanish'),
+(4, 'French'),
+(5, 'Italian'),
+(6, 'Japonese'),
+(7, 'Chinese'),
+(8, 'German');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `movie`
+--
+
+DROP TABLE IF EXISTS `movie`;
+CREATE TABLE IF NOT EXISTS `movie` (
+  `id_movie` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(254) NOT NULL,
+  `date` int NOT NULL,
+  `description` text NOT NULL,
+  `language` varchar(200) NOT NULL,
+  `link_trailer` text NOT NULL,
+  `movie_file` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `id_user` int NOT NULL,
+  PRIMARY KEY (`id_movie`),
+  KEY `id_user` (`id_user`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id_user` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(60) NOT NULL,
+  `email` varchar(120) NOT NULL,
+  `password` varchar(120) NOT NULL,
+  `admin` tinyint(1) DEFAULT NULL,
+  `super` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id_user`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `acting`
+--
+ALTER TABLE `acting`
+  ADD CONSTRAINT `acting_ibfk_1` FOREIGN KEY (`id_actor`) REFERENCES `actor` (`id_actor`),
+  ADD CONSTRAINT `acting_ibfk_2` FOREIGN KEY (`id_movie`) REFERENCES `movie` (`id_movie`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `contain`
+--
+ALTER TABLE `contain`
+  ADD CONSTRAINT `contain_ibfk_1` FOREIGN KEY (`id_movie`) REFERENCES `movie` (`id_movie`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `contain_ibfk_2` FOREIGN KEY (`id_category`) REFERENCES `category` (`id_category`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `movie`
+--
+ALTER TABLE `movie`
+  ADD CONSTRAINT `movie_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
